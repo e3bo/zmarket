e29eed8b0ccd8268d755a9a64d17a3fb27622afe
Eamon O'Dea <odea35@gmail.com>
2021-06-04 03:23:36 +0000

    Add data files

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Responses to survey questions related to sleep and socio-demographic behavior
+#'
+#' A dataset from a Gradient Metrics survey designed to identify a group of individuals likely to download an app designed to improve sleep.
+#'
+#' @format A data frame with 892 rows and 100 variables:
+#' @source \url{https://docs.google.com/document/d/1NpkDqtKKxesKislnQuTwsdixwpic-REHmByeAeRFjhs/edit}
+"survey_data"
+
+#' Responses to survey experiment about messages that affect likelihood of download
+#'
+#' A dataset from a Gradient Metrics survey experiment designed to determine the effects of attributes of a message on the motivation to download.
+#'
+#' @format A data frame with 10,704 rows and 9 variables:
+#' @source \url{https://www.gradientmetrics.com}
+"experiment_data"
